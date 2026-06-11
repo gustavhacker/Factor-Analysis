03cff5a89c91f5e3bea24614efa6eb36bbbb6f52
Gustav Bergkvist <gustav@rpm.se>
2026-06-11 13:32:07 +0200

    Lagt in merge logik, bästa steg fixa med priser

--- main.py
+++ main.py
@@ -22,13 +22,26 @@ if __name__ == "__main__":
         .merge(df_cash, on=["Ticker", "Fiscal Year", "Fiscal Period"], how="inner")
         .merge(df_balance, on=["Ticker", "Fiscal Year", "Fiscal Period"], how="inner")
     )
+    df_merged["Publish Date"] = pd.to_datetime(df_merged["Publish Date"])
 
     TICKERS = list(df_merged["Ticker"].unique())
     
     df_prices = pd.read_csv("./data/prices.csv", index_col=0, parse_dates=True)
     df_prices_clean = df_prices.dropna(axis=1, how="any")
     
+    prices_long = (
+        df_prices_clean.reset_index()
+          .melt(id_vars='Date',
+                var_name='Ticker',
+                value_name='Price')
+    )
+
     #TODO:  Använd Publish Date för matchning med pris/inputs 
+    prices_long = prices_long.sort_values(by=["Date", "Ticker"])
+    df_merged = df_merged.sort_values(by=["Publish Date", "Ticker"])
+
+    df = pd.merge_asof(df_merged, prices_long, left_on="Publish Date", right_on="Date", by="Ticker", direction="backward")
+    print(df[df["Ticker"] == "AAPL"]["Publish Date"])
 
     #TODO: Implementera ratios och LTM-kolumner för rådata som aggregeras (kassaflöde, vinst, omsättning exv.)
     # Bygg sedan 
